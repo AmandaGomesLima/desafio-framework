6112351903bf570ad5774093faf6c378ebfc42f0
AmandaGomesLima <69551721+AmandaGomesLima@users.noreply.github.com>
2021-03-05 19:31:23 -0300

    Adição dos arquivos do desafio

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Desafio Frame - Como se tornar um padawan</title>
+        <meta charset="utf-8" />
+        <meta name="author" content="Amanda Lima"/>
+        <link rel="stylesheet" type="text/css" href="estilo.css"/>
+    </head>
+    <body class="menu">
+        <div id="wrap">
+            <h1>DESAFIO TÉCNICO PADAWANS</h1>
+            <nav>
+                <ul>
+                    <li><a href="postagens.php" class="btn">POSTAGENS</a></li>
+                    <li><a href="albuns.php" class="btn">ALBUNS</a></li>
+                    <li><a href="todos.php" class="btn">TO-DOs</a></li>
+                </ul>
+            </nav> 
+        </div>
+    </body>
+</html>
---
+++ postagens.php
@@ -0,0 +1,40 @@
+<?php
+    $url = 'https://jsonplaceholder.typicode.com/posts/';
+    $json = file_get_contents($url);
+    $obj = json_decode($json);
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Desafio Frame - Como se tornar um padawan</title>
+        <meta charset="utf-8" />
+        <meta name="author" content="Amanda Lima"/>
+        <link rel="stylesheet" type="text/css" href="estilo.css"/>
+    </head>
+    <body>
+        <div id="wrap">
+            <div class="barra-superior">
+                <a href="index.php" class="btn btn-voltar">Voltar</a>
+            </div>
+            <h1>POSTAGENS</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>userId</th>
+                        <th>title</th>
+                        <th>body</th>
+                    </tr>
+                </thead>
+                <?php foreach ($obj as $dados) : ?>
+                <tr>
+                    <td><?php echo $dados->id; ?></td>
+                    <td><?php echo $dados->userId; ?></td>
+                    <td><?php echo $dados->title; ?></td>
+                    <td><?php echo $dados->body; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </body>
+</html>
